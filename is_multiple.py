9afd25239b6034f57611475066708aa251ec69db
parisnk <parisbarzin@gmail.com>
2018-09-11 22:11:18 -0400

    Added python code

---
+++ is_multiple.py
@@ -0,0 +1,13 @@
+#Defining the function called is_multiple
+def is_multiple(m, n):
+    # checking if the remainder of dividing n by m is zeroo
+    if n % m == 0:
+        # if the remainder is zero, return true
+        print("Is multiple")
+        # otherwise returns false
+    else:
+        print("Not multiple")
+        
+    
+#calling function by assining arguments 3, 9  
+is_multiple(4, 9)    
